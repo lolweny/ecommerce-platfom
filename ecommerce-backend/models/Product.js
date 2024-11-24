18a61288cab44d6f33f4dbf878c9d9b071e4aa7d
lanceolweny <lanceolweny@gmail.com>
2024-11-23 19:13:17 -0600

    updated

--- ecommerce-backend/models/Product.js
+++ ecommerce-backend/models/Product.js
@@ -2,9 +2,12 @@ const Sequelize = require('sequelize');
 const sequelize = require('../database');  // Import the Sequelize instance
 
 const Product = sequelize.define('product', {
-  name: { type: Sequelize.STRING },
-  price: { type: Sequelize.FLOAT },
-  stock: { type: Sequelize.INTEGER }
+  name: { type: Sequelize.STRING, allowNull: true },
+  price: { type: Sequelize.FLOAT, allowNull: true },
+  stock: { type: Sequelize.INTEGER, allowNull: true },
+  description: { type: Sequelize.TEXT, allowNull: true }, // Add description field
+  createdAt: { type: Sequelize.DATE, allowNull: false, defaultValue: Sequelize.NOW },
+  updatedAt: { type: Sequelize.DATE, allowNull: false, defaultValue: Sequelize.NOW },
 });
 
 module.exports = Product;
